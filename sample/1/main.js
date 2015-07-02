153b2f0772aaccef9c0239dab4066199983e2af8
WQ <niuyiwangqun@sohu.com>
2015-07-02 11:24:21 +0800

    增添播放音乐功能

---
+++ sample/1/main.js
@@ -0,0 +1,37 @@
+/**
+ * Created by WQ on 2015/7/2.
+ */
+
+main( function () {
+    var insertCSS = imports( "../../lib/css.js" ).insertCSSRules;
+    var Scence = imports( "../../lib/cinema.js" ).Scence;
+    var util = imports( "../../lib/util.js" );
+    var sys = imports( "../../lib/sys/build.js" );
+
+    var TimeLine = [];
+    var scences = document.querySelectorAll( ".scence" );
+
+    insertCSS( {
+        ".bg.b-bg-mask" : {
+            width : document.body.offsetHeight / 1008 * 1417 + "px"
+        }
+    } );
+
+    var musics = ["audio/1.mp3", "audio/2.mp3", "audio/3.mp3"];
+
+    // 组建TimeLine
+    util.loopArray( scences, function ( s, i ) {
+        TimeLine.push( new Scence( {
+            el : s,
+            lifeTime : 4,
+            audio : {
+                src : musics[i],
+                delay : 0,
+                isJust : true
+            }
+        } ) );
+        document.body.removeChild( s );
+    } );
+
+    sys.play( TimeLine );
+} );
