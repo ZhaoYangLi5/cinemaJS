001ee3259a95508cf66678e303b7dc9e3d1723d9
WQ <niuyiwangqun@sohu.com>
2015-07-06 15:35:01 +0800

    新的示例代码

--- sample/3/main.js
+++ sample/3/main.js
@@ -12,9 +12,47 @@ main( function () {
     util.loopArray( scences, function ( s ) {
         TimeLine.push( new Scence( {
             el : s,
-            lifeTime : 6
+            lifeTime : 8
         } ) );
         document.body.removeChild( s );
     } );
-    sys.play( TimeLine, null, null );
+
+    var subtitle = [
+        {
+            text : "风吹落最后一片叶",
+            delay : 1.55
+        },
+        {
+            text : "我的心也飘着雪",
+            delay : 6.52
+        },
+        {
+            text : "爱只能往回忆里堆叠",
+            delay : 5.82
+        },
+        {
+            text : "oh~　给下个季节",
+            delay : 4.39
+        },
+        {
+            text : "忽然间树梢冒花蕊",
+            delay : 6.87
+        },
+        {
+            text : "我怎么会都没有感觉",
+            delay : 5.4
+        },
+        {
+            text : "oh~　整条街　都是恋爱的人",
+            delay : 4.55
+        },
+        {
+            text : "我独自走在暖风的夜",
+            delay : 6.78
+        }, {
+            text : "多想要向过去告別",
+            delay : 9.14
+        }
+    ];
+    sys.play( TimeLine, subtitle, null );
 } );
